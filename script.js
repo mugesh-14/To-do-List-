3c6b21722a5a5750f0a872a5c34fb01a92bbab5d
Mugesh <mugesh1453@gmail.com>
2025-09-15 09:34:31 +0530

    alter : js to remove task and css to line through the task

--- script.js
+++ script.js
@@ -10,8 +10,8 @@ function addTask() {
         li.innerHTML = inputBox.value;
         listContainer.appendChild(li);
         let span = document.createElement("span")
-        span.inn
-        listContainer.appendChild(span);
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
     }
     inputBox.value = "";
     saveData();
